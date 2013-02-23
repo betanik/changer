760b9aec182e0bcbe322992204599d530670d249
Nathanael Ng <non@nonat.com>
2013-02-23 14:46:31 +0000

    First cut

---
+++ changer/src/main/java/net/nat/changer/web/ChangeController.java
@@ -0,0 +1,15 @@
+package net.nat.changer.web;
+
+import net.nat.changer.domain.Change;
+import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/changes")
+@Controller
+@RooWebScaffold(path = "changes", formBackingObject = Change.class)
+public class ChangeController {
+	public Change initializeChange() {
+		return new Change();
+	}
+}
---
+++ changer/src/main/java/net/nat/changer/web/PersonController.java
@@ -0,0 +1,12 @@
+package net.nat.changer.web;
+
+import net.nat.changer.domain.Person;
+import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/people")
+@Controller
+@RooWebScaffold(path = "people", formBackingObject = Person.class)
+public class PersonController {
+}
